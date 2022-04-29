c487683c18f79df795ac576937ba3b2469e23f2d
Aghababayev <aghababayev52@gmail.com>
2022-04-29 09:50:49 +0400

    last

---
+++ Overtime_Core_WEB/Controllers/OvertimeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Overtime_Core_WEB.Controllers
+{
+    public class OvertimeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
